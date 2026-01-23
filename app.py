3b309ecd0e4d2b8269f9e234f8731d80809f4ae9
Mahek <mahekaggarwal05@gmail.com>
2026-01-23 12:35:34 +0530

    update

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    #return "hey this is first line of first page"
+    return redirect((url_for("index")))
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug = True)
