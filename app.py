9b0c0e42e9e9f38ea7b4e7a1247e22eb4445a005
Mahek <mahekaggarwal05@gmail.com>
2026-02-10 21:35:39 +0530

    update

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import pymysql
 from config import Config
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.secret_key = app.config["SECRET_KEY"]
 
-# MySQL connection function
+
 def get_db_connection():
     return pymysql.connect(
         host=app.config["DB_HOST"],
@@ -25,13 +26,13 @@ def login():
         conn = get_db_connection()
         cursor = conn.cursor()
 
-        query = "SELECT * FROM users WHERE username=%s AND password=%s"
-        cursor.execute(query, (username, password))
+        query = "SELECT * FROM users WHERE username=%s"
+        cursor.execute(query, (username,))
         user = cursor.fetchone()
 
         conn.close()
 
-        if user:
+        if user and check_password_hash(user["password"], password):
             session["user"] = user["username"]
             return redirect(url_for("dashboard"))
         else:
@@ -52,12 +53,13 @@ def logout():
     session.pop("user", None)
     return redirect(url_for("login"))
 
-
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
+
+        hashed_password = generate_password_hash(password)
 
         conn = get_db_connection()
         cursor = conn.cursor()
@@ -71,15 +73,15 @@ def signup():
 
         cursor.execute(
             "INSERT INTO users (username, password) VALUES (%s, %s)",
-            (username, password)
+            (username, hashed_password)
         )
         conn.commit()
         conn.close()
 
-        return redirect(url_for("login"))
+        session["user"] = username
+        return redirect(url_for("dashboard"))
 
     return render_template("signup.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
--- config.py
+++ config.py
@@ -4,3 +4,5 @@ class Config:
     DB_USER = "root"
     DB_PASSWORD = "Mahek@2005"
     DB_NAME = "flask_auth"
+    DB_PORT = 3306
+    
